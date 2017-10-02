ed63d9b36675bd07339e409665c15a5af64aa90f
Emaltard <maxence.joulin@gmail.com>
2017-10-02 21:17:22 +0200

    Correction TD2 exo3

--- TD2/exo3.c
+++ TD2/exo3.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct element {
-        int valeur;
+        char valeur;
         struct element* suivant;
 };
 typedef struct element ELEMENT;
 
+void ajoutElement(ELEMENT* chaine, char c){
+  ELEMENT* tmp = chaine;
+  while(tmp->suivant!=NULL) {
+    tmp = tmp->suivant;
+  }
+  ELEMENT* new_elem = malloc(sizeof(ELEMENT*));
+  new_elem->valeur = c;
+  tmp->suivant = new_elem;
+}
+
+void enleveElement(ELEMENT* chaine, char c){
+  ELEMENT * act = chaine;
+  while(act->suivant->valeur != c){
+    act = act->suivant;
+  }
+  ELEMENT * tmp = act->suivant;
+  act->suivant = tmp->suivant;
+  free(tmp);
+}
+
 
 void afficheChaine(ELEMENT* chaine){
         ELEMENT* tmp = chaine;
-        printf("%d\n", tmp->valeur);
+        printf("%c", tmp->valeur);
         while(tmp->suivant!=NULL) {
                 tmp = tmp->suivant;
-                printf("%d\n", tmp->valeur);
+                printf("%c", tmp->valeur);
         }
         printf("\n");
 }
@@ -20,7 +41,11 @@ void afficheChaine(ELEMENT* chaine){
 
 int main(){
         ELEMENT elem;
-        elem.valeur = 3;
+        elem.valeur = '3';
         elem.suivant = NULL;
+
+        ajoutElement(&elem, 'o');
+        ajoutElement(&elem, 'w');
+        enleveElement(&elem, 'o');
         afficheChaine(&elem);
 }
