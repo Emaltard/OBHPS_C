247898f0c706b2dad0233ed3ccabaf58ebf620ca
Maxence Joulin <maxence.joulin@gmail.com>
2017-10-06 17:58:54 +0200

    Debut controle TD2

--- TD2/exo3.c
+++ TD2/exo3.c
@@ -7,45 +7,77 @@ struct element {
 };
 typedef struct element ELEMENT;
 
-void ajoutElement(ELEMENT* chaine, char c){
-  ELEMENT* tmp = chaine;
-  while(tmp->suivant!=NULL) {
-    tmp = tmp->suivant;
-  }
-  ELEMENT* new_elem = malloc(sizeof(ELEMENT*));
-  new_elem->valeur = c;
-  tmp->suivant = new_elem;
+ELEMENT* ajoutElement(ELEMENT* chaine, char c){
+        if(chaine==NULL) {
+                chaine = malloc(sizeof(ELEMENT));
+                chaine->suivant = NULL;
+                chaine->valeur = c;
+        }else{
+                chaine->suivant =  ajoutElement(chaine->suivant,c);
+        }
+        return chaine;
 }
 
-void enleveElement(ELEMENT* chaine, char c){
-  ELEMENT * act = chaine;
-  while(act->suivant->valeur != c){
-    act = act->suivant;
-  }
-  ELEMENT * tmp = act->suivant;
-  act->suivant = tmp->suivant;
-  free(tmp);
+ELEMENT* enleveElement(ELEMENT* chaine, char c){
+        if(chaine!=NULL) {
+                if(chaine->valeur == c) {
+                        ELEMENT* tmp = chaine;
+                        chaine = chaine->suivant;
+                        free(tmp);
+                        return chaine;
+                }
+                else{
+                        chaine->suivant = enleveElement(chaine->suivant, c);
+                }
+        }
+        return chaine;
 }
 
 
 void afficheChaine(ELEMENT* chaine){
         ELEMENT* tmp = chaine;
-        printf("%c", tmp->valeur);
-        while(tmp->suivant!=NULL) {
+        while(tmp!=NULL) {
+                printf("%c -> ", tmp->valeur);
                 tmp = tmp->suivant;
-                printf("%c", tmp->valeur);
         }
-        printf("\n");
+        printf("NULL\n");
 }
 
 
 int main(){
-        ELEMENT elem;
-        elem.valeur = '3';
-        elem.suivant = NULL;
 
-        ajoutElement(&elem, 'o');
-        ajoutElement(&elem, 'w');
-        enleveElement(&elem, 'o');
-        afficheChaine(&elem);
+        //INITIALISATION LISTE
+        ELEMENT* elem =NULL;
+        elem = ajoutElement(elem, 'm');
+        elem = ajoutElement(elem, 'a');
+        elem = ajoutElement(elem, 'x');
+        elem = ajoutElement(elem, 'e');
+        elem = ajoutElement(elem, 'n');
+        elem = ajoutElement(elem, 'c');
+        elem = ajoutElement(elem, 'e');
+
+        elem = ajoutElement(elem, ' ');
+
+        elem = ajoutElement(elem, 'j');
+        elem = ajoutElement(elem, 'o');
+        elem = ajoutElement(elem, 'u');
+        elem = ajoutElement(elem, 'l');
+        elem = ajoutElement(elem, 'i');
+        elem = ajoutElement(elem, 'n');
+
+        //1er Affichage
+        afficheChaine(elem);
+
+
+        //DESTRUCTION LISTE
+
+        ELEMENT* tmp;
+        while(elem !=NULL) {
+                tmp = elem;
+                elem = elem->suivant;
+                free(tmp);
+                tmp = tmp->suivant;
+        }
+
+        return 0;
 }
